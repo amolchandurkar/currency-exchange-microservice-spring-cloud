9e217eee61333ded3ce8d8c6d288278a51b23344
Chandurkar, Amol <Amol.Chandurkar@fisglobal.com>
2017-10-23 22:11:36 +0530

    Modification for naming service

--- currency-conversion-service/src/main/java/com/aac/spring/cloud/ms/currencyservice/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ currency-conversion-service/src/main/java/com/aac/spring/cloud/ms/currencyservice/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
+@EnableFeignClients("com.aac.spring.cloud.ms.currencyservice.currencyconversionservice")
 @EnableDiscoveryClient
 public class CurrencyConversionServiceApplication {
 
